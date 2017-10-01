45a3407105a91f45bd055c0e1a3f52a64be31bd8
Rohan Upponi <rponi70@gmail.com>
2017-10-01 01:41:28 -0700

    Application can now read and play .wav files using JMF framework, albeit without controls to play or stop the music once run.

--- MP3_Player/src/MP3_Player.java
+++ MP3_Player/src/MP3_Player.java
@@ -1,15 +1,34 @@
-import javax.sound.sampled.*;
-import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.media.Manager;
+import javax.media.NoPlayerException;
+import javax.media.CannotRealizeException;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MP3_Player {
     public static void main(String[] args) {
         try {
-            AudioFileFormat inputFileFormat = AudioSystem.getAudioFileFormat(new File("ImABoss.mp3"));
+            File songFile = new File("C://Java Projects/MusicPlayer/ImABoss.wav");
+            System.out.println(AudioSystem.getAudioFileFormat(songFile));
 
-        } catch(UnsupportedAudioFileException ax) {
-            ax.printStackTrace();
-        } catch(IOException ex) {
+            Path songPath = Paths.get("C:","Java Projects/MusicPlayer/ImABoss.wav");
+            URL songURL = songPath.toUri().toURL();
+            Manager.createRealizedPlayer(songURL).start();
+        } catch (MalformedURLException mx) {
+            mx.printStackTrace();
+        } catch (IOException ex) {
             System.out.println(ex);
+        } catch (NoPlayerException nx) {
+            nx.printStackTrace();
+        } catch (CannotRealizeException cx) {
+            cx.printStackTrace();
+        } catch (UnsupportedAudioFileException ux) {
+            ux.printStackTrace();
         }
     }
 }
