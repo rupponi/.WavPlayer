d551e0c36d0237dbb5443f8cf2dd4b98d69641f8
Rohan Upponi <rponi70@gmail.com>
2017-10-16 13:49:12 -0700

    Documentation update: - Documentation added for the MusicPlayerFrontPanel class.

--- MP3_Player/src/MusicPlayerFrontPanel.java
+++ MP3_Player/src/MusicPlayerFrontPanel.java
@@ -40,9 +40,11 @@ public class MusicPlayerFrontPanel extends Application{
 
     public void start(Stage musicStage) {
 
+        //SET THE STAGE
         musicStage.setTitle(".WavPlayer Music Player");
         musicStage.initStyle(StageStyle.TRANSPARENT);
 
+        //CONTAINERS FOR ENTIRE FRONT PANEL LAYOUT
         VBox songTimer = new VBox();
         VBox buttonInterFace = new VBox();
         HBox cornerButtons = new HBox();
@@ -52,9 +54,14 @@ public class MusicPlayerFrontPanel extends Application{
         Label startTime = new Label();
         Label endTime = new Label();
 
+
+        //*******FILE BUTTON*******//
         Button fileButton = new Button();
         fileButton.setText("Select Song");
+        fileButton.setStyle("-fx-background-color: linear-gradient(#4d4d4e,#0a0a0a)");
         fileButton.setTextFill(Color.WHITE);
+
+        //FILE BUTTON LISTENER
         fileButton.setOnAction(new EventHandler<ActionEvent>() {
            public void handle(ActionEvent fileAccess) {
                while (!selector.importFinished) {
@@ -80,11 +87,9 @@ public class MusicPlayerFrontPanel extends Application{
                }
            }
         });
-        fileButton.setStyle("-fx-background-color: linear-gradient(#4d4d4e,#0a0a0a)");
 
 
         //******* PLAY BUTTON *******//
-
         Button playButton = new Button();
         Image playImage = new Image(getClass().getResourceAsStream("play.png"));
         ImageView playImageView = new ImageView(playImage);
@@ -92,6 +97,7 @@ public class MusicPlayerFrontPanel extends Application{
         playImageView.setFitWidth(50.0);
         playButton.setGraphic(playImageView);
 
+        //SPACE TO CONTAIN THE PLAY.PNG / PAUSE.PNG IMAGES FOR BUTTON
         Circle playCircle = new Circle();
         playCircle.setRadius(50);
         playCircle.maxHeight(40);
@@ -103,9 +109,10 @@ public class MusicPlayerFrontPanel extends Application{
         playButton.setStyle("-fx-background-color: #0a0a0a");
         playButton.setTextFill(Color.WHITE);
 
+        //PLAY BUTTON LISTENER
         playButton.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent playClicked) {
-                if (controller.mp3Player.getSongPlayer() != null) {
+                if (controller.mp3Player.getSongPlayer() != null) {//IF THERE IS A LOADED SONG, PLAYER WILL NOT BE NULL.
                     if ((controller.mp3Player.getSongPlayer().getState() != Controller.Started)) {
                         controller.play();
                         Image pauseView = new Image(getClass().getResourceAsStream("pause.png"));
@@ -114,7 +121,7 @@ public class MusicPlayerFrontPanel extends Application{
                         pauseImageView.setFitWidth(50.0);
                         playButton.setGraphic(pauseImageView);
                     }
-                    else if (controller.mp3Player.getSongPlayer().getState() == Controller.Started) {
+                    else if (controller.mp3Player.getSongPlayer().getState() == Controller.Started) {//IF PLAYER IS IN STARTED STATE, RE-CLICK OF PLAY BUTTON WILL PAUSE OPERATION.
                         controller.pause();
 
                         Image resumeView = new Image(getClass().getResourceAsStream("play.png"));
@@ -123,7 +130,7 @@ public class MusicPlayerFrontPanel extends Application{
                         resumeImageView.setFitWidth(50.0);
                         playButton.setGraphic(resumeImageView);
                     }
-                } else {
+                } else {//IF SONG IS NOT PICKED, POST ALERT THAT USER MUST PICK A SONG FIRST.
                     Alert noSongWarning = new Alert(Alert.AlertType.WARNING);
                     noSongWarning.setTitle("I Need a Song");
                     noSongWarning.setContentText("Please select a song.");
@@ -133,61 +140,63 @@ public class MusicPlayerFrontPanel extends Application{
         });
 
 
-
+        //*******MINIMIZE BUTTON*******//
         Button minimizeButton = new Button();
         minimizeButton.setText("-");
+        minimizeButton.setMinHeight(25);
+        minimizeButton.setMinWidth(50);
+        minimizeButton.setStyle("-fx-background-color: #4d4d4e");
+        minimizeButton.setTextFill(Color.WHITE);
+
+        //MINIMIZE BUTTON LISTENER
         minimizeButton.setOnAction(new EventHandler<ActionEvent>() {
            public void handle(ActionEvent minimizeWindow) {
                musicStage.setIconified(true);
            }
         });
 
-        minimizeButton.setMinHeight(25);
-        minimizeButton.setMinWidth(50);
-        minimizeButton.setStyle("-fx-background-color: #4d4d4e");
-        minimizeButton.setTextFill(Color.WHITE);
 
-
-
+        //*******FULL SCREEN BUTTON*******//
         Button fullScreenButton = new Button();
         fullScreenButton.setText("▭");
-        fullScreenButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent fullScreen) {
-                if (!musicStage.isMaximized()) {
-                    musicStage.setMaximized(true);
-                } else {
-                    musicStage.setMaximized(false);
-                }
-            }
-        });
-
         fullScreenButton.setMinHeight(25);
         fullScreenButton.setMinWidth(50);
         fullScreenButton.setTextFill(Color.WHITE);
         fullScreenButton.setStyle("-fx-background-color: #4d4d4e");
 
-
-
-
-        Button exitButton = new Button();
-        exitButton.setText("x");
-        exitButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent exitClicked) {
-                musicStage.hide();
-                if (controller.mp3Player.getSongPlayer() != null) {
-                    controller.mp3Player.getSongPlayer().stop();
+        //FULL SCREEN BUTTON LISTENER//
+        fullScreenButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent fullScreen) {
+                if (!musicStage.isMaximized()) {//Case: Screen isn't in maximized state yet.
+                    musicStage.setMaximized(true);
+                } else {//Case: Screen is already maximized.
+                    musicStage.setMaximized(false);
                 }
-                System.exit(0);
             }
         });
 
+
+        //*******EXIT BUTTON*******//
+        Button exitButton = new Button();
+        exitButton.setText("x");
         exitButton.setMinHeight(20);
         exitButton.setMinWidth(50);
         exitButton.setStyle("-fx-background-color: #ff0000");
         exitButton.setTextFill(Color.WHITE);
 
+        //EXIT BUTTON LISTENER//
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent exitClicked) {
+                musicStage.hide();//Close musicStage view.
+                if (controller.mp3Player.getSongPlayer() != null) {//Assuming song was selected, so there is a realized player present.
+                    controller.mp3Player.getSongPlayer().stop();//Closes the realized player.
+                }
+                System.exit(0);//Clean exit.
+            }
+        });
 
 
+        //This VBox songTimer contains the timerBox with the slider and labels of startTime and endTime as well as the timer box that holds the song time.
         songTimer.setAlignment(Pos.BASELINE_CENTER);
         songTimer.setPadding(new Insets(15,20,15,20));
         songTimer.setSpacing(15);
@@ -198,6 +207,7 @@ public class MusicPlayerFrontPanel extends Application{
         endTime.setText(String.format("%d:%02d",controller.mp3Player.getSongTime()/60,controller.mp3Player.getSongTime()-(controller.mp3Player.getSongTime()/60)*60));
         endTime.setTextFill(Color.GRAY);
 
+        //This timer box will hold the current song time progression.
         TextArea timer = controller.mp3Player.getTimer();
         timer.setMinHeight(45);
         timer.setMinWidth(100);
@@ -207,23 +217,28 @@ public class MusicPlayerFrontPanel extends Application{
         timerBox.setAlignment(Pos.BASELINE_CENTER);
         timerBox.getChildren().addAll(startTime,controller.mp3Player.getTimeSlider(),endTime);
 
+        //Formats panel holding the custom minimize, fullscreen, and close buttons on the top right.
         cornerButtons.setAlignment(Pos.TOP_RIGHT);
         cornerButtons.setMaxHeight(30);
         cornerButtons.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.NONE,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
         cornerButtons.setStyle("-fx-background-color: #4d4d4e");
         cornerButtons.getChildren().addAll(minimizeButton,fullScreenButton,exitButton);
 
+        //Add the slider and labels along with the song timer text bos to the songTimer VBox. They will be put in vertical order.
         songTimer.getChildren().addAll(timerBox,timer);
 
         controller.mp3Player.getTimer().setText(String.format("0:00/%d:%02d",controller.mp3Player.getSongTime()/60,controller.mp3Player.getSongTime()-(controller.mp3Player.getSongTime()/60)*60));
 
+        //Formats the box that holds the play button and file access button. They will be put in a horizontal order.
         buttonInterFace.setAlignment(Pos.BASELINE_CENTER);
         buttonInterFace.setPadding(new Insets(15,20,15,20));
         buttonInterFace.setSpacing(30);
         buttonInterFace.getChildren().addAll(playButton,fileButton);
 
+        //Container holds both the songTimer box and buttonInterface box. They will be ordered vertically.
         container.getChildren().addAll(songTimer,buttonInterFace);
 
+        //This BorderPane is what is seen on the application. The top section holds the corner buttons and the center holds the content play and access buttons as well as the timer and slider.
         frontPanel.setTop(cornerButtons);
         frontPanel.setCenter(container);
         frontPanel.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
